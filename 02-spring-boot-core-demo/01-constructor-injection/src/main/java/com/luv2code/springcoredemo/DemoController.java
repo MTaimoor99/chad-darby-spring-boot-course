027d3cc38d83fcfe4eb2237f00aa4cacf9143ec8
Muhammad Taimoor <taimoor992002@gmail.com>
2025-08-02 17:10:43 +0500

    Created rest controller for daily workout that involves result of cricket coach.

--- 02-spring-boot-core-demo/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/DemoController.java
+++ 02-spring-boot-core-demo/01-constructor-injection/src/main/java/com/luv2code/springcoredemo/DemoController.java
@@ -2,6 +2,7 @@ package com.luv2code.springcoredemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +12,14 @@ public class DemoController {
 
     //define a constructor for dependency injection
     //autowired tells spring to inject the dependency.
+    //We have just one constructor, so in this case, autowired is optional.
     @Autowired
     public DemoController(Coach theCoach){
         myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
     }
 }
