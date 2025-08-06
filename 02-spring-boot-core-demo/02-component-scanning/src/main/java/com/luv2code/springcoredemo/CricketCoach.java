5ef2e300f1329a018181db63037714139077b263
Muhammad Taimoor <muhammad.taimoor@jintech.com>
2025-08-06 14:51:35 +0500

    created a new project to learn component scanning.

---
+++ 02-spring-boot-core-demo/02-component-scanning/src/main/java/com/luv2code/springcoredemo/CricketCoach.java
@@ -0,0 +1,12 @@
+package com.luv2code.springcoredemo;
+
+import org.springframework.stereotype.Component;
+
+//Mark the class as a Spring bean using this annotation.
+@Component
+public class CricketCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+}
