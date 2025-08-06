dd9eb436dacdfdc78c63132ca19ccb7c4654c5da
Muhammad Taimoor <muhammad.taimoor@jintech.com>
2025-08-06 15:00:33 +0500

    Divided my files into packages. This was done to demonstrate component scanning. State before moving packages around and seeing how Spring handles them.

--- 02-spring-boot-core-demo/02-component-scanning/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-core-demo/02-component-scanning/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -1,10 +1,10 @@
-package com.luv2code.springcoredemo;
+package com.luv2code.springcoredemo.common;
 
 import org.springframework.stereotype.Component;
 
 //Mark the class as a Spring bean using this annotation.
 @Component
-public class CricketCoach implements Coach{
+public class CricketCoach implements Coach {
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes";
